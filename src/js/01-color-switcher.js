bee52f1bd21eebf71fdeac486ef6c9f078fa71eb
Vladislav Zmaga <vladislavzmaga@gmail.com>
2022-08-26 00:50:21 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.disabled = false;
+
+startBtn.addEventListener('click', onUpdateBodyBackground);
+stopBtn.addEventListener('click', stopUpdateBodyColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onUpdateBodyBackground() {
+  startBtn.disabled = true;
+  timerId = setInterval(onBodyBackground, 1000);
+}
+
+function onBodyBackground() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function stopUpdateBodyColor() {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,4 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
