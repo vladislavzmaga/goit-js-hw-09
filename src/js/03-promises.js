e712131c012de237dc3a6dbbd37f04d759589de8
Vladislav Zmaga <vladislavzmaga@gmail.com>
2022-08-30 00:03:02 +0300

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const form = document.querySelector('.form');
+const delay = form.elements.delay;
+const step = form.elements.step;
+const amount = form.elements.amount;
+
+form.addEventListener('submit', generatedPromise);
+
+function generatedPromise(evt) {
+  evt.preventDefault();
+  const countAmount = +amount.value;
+  const countStep = +step.value;
+  let countDelay = +delay.value;
+  for (let i = 1; i <= countAmount; i += 1) {
+    createPromise(i, countDelay)
+      .then(result => Notiflix.Notify.success(result))
+      .catch(error => Notiflix.Notify.failure(error));
+    countDelay += countStep;
   }
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+  return promise;
 }
